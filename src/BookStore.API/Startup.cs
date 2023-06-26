37b304067128564b985713e0fd455be8db8762a9
LustaY <lusta.yu.a@gmail.com>
2023-06-26 17:01:35 +0300

    2nd commit DbContext error

---
+++ src/BookStore.API/Startup.cs
@@ -0,0 +1,33 @@
+﻿//using AutoMapper;
+//using BookStore.API.Configuration;
+using BookStore.Infrastructure.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace BookStore.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            services.AddDbContext<BookStoreDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddControllers();
+
+        }
+    }
+}
